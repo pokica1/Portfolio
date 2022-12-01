39891c1d3c843b822adf317654490cee3296e3f2
OrsolyaToro <k1916789@kingston.ac.uk>
2022-12-01 17:28:24 +0000

    home page, contact button

---
+++ src/Button/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button.css";
+
+export const Button = (props) => (
+  <div className="button-wrapper" id={props.id}>
+    {props.text}
+  </div>
+);
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { PageContent } from '../../PageContent/PageContent';
+import { NavLink } from "react-router-dom";
+import { Button } from '../../Button/Button';
+import './HomePage.css';
 
 const HomePage = () => (
     <PageContent>
-        <h1>Welcome to the Home Page</h1>
+        <div className="container">
+            <div className='article'>This is an article</div>
+            <div className='image'><img src='homePageImage.svg' alt="women with a laptop"/></div>
+        </div>
+        <NavLink to="/contact">
+            <Button text="Contact"/>
+        </NavLink>
     </PageContent>   
 )
 
