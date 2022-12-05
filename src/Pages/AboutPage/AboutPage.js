2d52acc41cb54f8211f0210fdf89f86d030b378d
OrsolyaToro <k1916789@kingston.ac.uk>
2022-12-05 17:43:09 +0000

    added profile pic to about page and article

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { PageContent } from '../../PageContent/PageContent';
+import './AboutPage.css'
 
 const AboutPage = () => (
     <PageContent>
-        <h1>Welcome to the About Page</h1>
+        <h1>About me</h1>
+        <div id='article-container'>
+            <div id='info'>This is an article</div>
+            <div className='image'><img src='profilePicLinkedin.jpg' alt="Orsolya smiling"/></div>
+        </div>
+        
     </PageContent>   
 )
 
