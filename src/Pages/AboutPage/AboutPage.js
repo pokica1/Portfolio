8353487481c728801dbb564de1cac83b0ccd52ad
OrsolyaToro <orsolya.kadar@yahoo.com>
2022-12-30 17:09:02 +0000

    aboutPage max-width changed

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -7,28 +7,28 @@ const AboutPage = () => (
 		<h1>About me</h1>
 		<div id="article-container">
 			<div id="info">
-				<p>
+				<p className="aboutParagraph">
 					I recently obtained a first-class degree in BSc Computer Science from
 					Kingston University.
 				</p>
-				<p>
+				<p className="aboutParagraph">
 					My final year project focused on creating a website for a client.
 					Throughout the project I have created the front end using, HTML, CSS,
 					JavaScript and React. I utilised AWS Amplify to help build out the
 					back end and connect it to the front end. A relational database has
 					been used to store data and used GraphQL to manage fetching it.
 				</p>
-				<p>
+				<p className="aboutParagraph">
 					During my university years, I have focused on learning mobile app
 					development, databases, UX/UI and programming mainly in Java.
 				</p>
-				<p>
+				<p className="aboutParagraph">
 					Also, I became an academic mentor to help students understand the
 					university material and help them with their workshops. I have worked
 					as a QA engineer intern and I have some experience with automation
 					testing using Cypress and some manual testing.
 				</p>
-				<p>
+				<p className="aboutParagraph">
 					In my free time, I enjoy going on walks with my two golden retrievers,
 					baking desserts and snowboarding in the winter.
 				</p>
--- src/Pages/SkillsPage/SkillsPage.js
+++ src/Pages/SkillsPage/SkillsPage.js
@@ -53,26 +53,26 @@ const SkillsPage = () => (
 			<div id="experience-container">
 				<img id="perlego-logo" src="Perlego.jpg" alt="P letter as a logo" />
 				<div id="description">
-					<h3>
+					<h3 className="skillsParagraph">
 						Perlego - April 2020 - September 2020 - Quality Assurance Engineer
 						Intern
 					</h3>
-					<p>
+					<p className="skillsParagraph">
 						I did automation testing with Cypress alongside some manual testing.
 						I worked in a small team following Agile methodology.
 					</p>
-					<p>
+					<p className="skillsParagraph">
 						My main responsibilities were to write tests for existing web pages
 						using Cypress; write tests for pages that were being built and to
 						help out with manual testing before a new release.
 					</p>
-					<p>
+					<p className="skillsParagraph">
 						Other day-to-day duties included keeping an eye on the testing
 						dashboard. After finding failed test on the dashboard, I had to look
 						into why is the test failing, fix the test or report to a developer
 						or create a ticket to fix the issue.
 					</p>
-					<p>
+					<p className="skillsParagraph">
 						Also, I have done some code reviews, to check other QA’s tests and
 						try to help when other QA team members were stuck on something.
 					</p>
