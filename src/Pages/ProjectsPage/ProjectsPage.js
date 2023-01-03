0513e888d256bc99df8d08c707110a29a65617d8
OrsolyaToro <orsolya.kadar@yahoo.com>
2023-01-03 15:46:48 +0000

    ProjectPage mobile and tablet friendly

--- src/Pages/ProjectsPage/ProjectsPage.js
+++ src/Pages/ProjectsPage/ProjectsPage.js
@@ -5,7 +5,22 @@ import "./ProjectsPage.css";
 
 const ProjectsPage = () => (
 	<PageContent>
+		<h1 id="projectPageTitle">Projects</h1>
 		<div id="projectPage-container">
+			<ProjectCard
+				src="logo512.png"
+				alt="React Native logo"
+				projectTitle="Reading Diary with React Native"
+				projectDescription="A small little app created with React Native. The app has vibrant colours as main target audiance is young children. Bla bla bla."
+				projectLink="https://github.com/pokica1"
+			/>
+			<ProjectCard
+				src="logo512.png"
+				alt="React Native logo"
+				projectTitle="Reading Diary with React Native"
+				projectDescription="A small little app created with React Native. The app has vibrant colours as main target audiance is young children. Bla bla bla."
+				projectLink="https://github.com/pokica1"
+			/>
 			<ProjectCard
 				src="logo512.png"
 				alt="React Native logo"
