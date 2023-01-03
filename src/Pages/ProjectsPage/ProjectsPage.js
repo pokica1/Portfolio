23ed3099e58d4c26946aaf06e4fb15818e68cae6
OrsolyaToro <orsolya.kadar@yahoo.com>
2023-01-03 11:35:33 +0000

    project card placeholder created

--- src/Pages/ProjectsPage/ProjectsPage.js
+++ src/Pages/ProjectsPage/ProjectsPage.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { PageContent } from "../../PageContent/PageContent";
+import { ProjectCard } from "../../ProjectCard/ProjectCard";
+import "./ProjectsPage.css";
 
 const ProjectsPage = () => (
 	<PageContent>
-		<h1>Welcome to the Projects Page</h1>
+		<div id="projectPage-container">
+			<ProjectCard
+				src="logo512.png"
+				alt="React Native logo"
+				projectTitle="Reading Diary with React Native"
+				projectDescription="A small little app created with React Native. The app has vibrant colours as main target audiance is young children. Bla bla bla."
+				projectLink="https://github.com/pokica1"
+			/>
+			<ProjectCard
+				src="logo512.png"
+				alt="React Native logo"
+				projectTitle="Reading Diary with React Native"
+				projectDescription="A small little app created with React Native. The app has vibrant colours as main target audiance is young children. Bla bla bla."
+				projectLink="https://github.com/pokica1"
+			/>
+		</div>
 	</PageContent>
 );
 
---
+++ src/ProjectCard/ProjectCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ProjectCard.css";
+import { FaGithub } from "react-icons/fa";
+
+export const ProjectCard = (props) => (
+	<div id="projectCard-container">
+		<div>
+			<img id="projectImage" src={props.src} alt={props.alt} />
+		</div>
+		<div id="projectTitle">{props.projectTitle}</div>
+		<p id="projectDescription">{props.projectDescription}</p>
+		<div id="link-container">
+			<p id="linkText">Check out the GitHub link here:</p>
+			<a href={props.projectLink} target="_blank" rel="noreferrer">
+				<FaGithub color="white" size="50px" />
+			</a>
+		</div>
+	</div>
+);
