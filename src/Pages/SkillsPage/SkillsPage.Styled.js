3120c457d335ac1dbc8c1f75b9cc3b7e37e104e3
OrsolyaToro <orsolya.kadar@yahoo.com>
2023-01-27 17:02:15 +0000

    skillsPage styled component

---
+++ src/Pages/SkillsPage/SkillsPage.Styled.js
@@ -0,0 +1,191 @@
+import styled from "styled-components/macro";
+import { DeviceSizes } from "../../Components/Styled/DeviceSizes";
+
+export const SkillsPageContainer = styled.div`
+	text-align: center;
+	max-width: 55%;
+	margin-left: auto;
+	margin-right: auto;
+	${DeviceSizes.laptop} {
+		max-width: 90%;
+	}
+	${DeviceSizes.tablet} {
+		max-width: 90%;
+	}
+	${DeviceSizes.mobile} {
+		max-width: 90%;
+	}
+`;
+
+export const SkillsTitle = styled.h1`
+	color: white;
+	padding-top: 50px;
+	margin-top: 0px;
+	${DeviceSizes.laptop} {
+		padding-bottom: 0;
+	}
+	${DeviceSizes.tablet} {
+		padding-bottom: 0;
+	}
+	${DeviceSizes.mobile} {
+		padding-bottom: 0;
+	}
+`;
+
+export const SkillsCardContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 15px;
+	justify-content: center;
+	margin-bottom: 40px;
+	${DeviceSizes.laptop} {
+		flex-wrap: wrap;
+	}
+	${DeviceSizes.tablet} {
+		flex-wrap: wrap;
+	}
+	${DeviceSizes.mobile} {
+		flex-wrap: wrap;
+	}
+`;
+
+export const EducationTitle = styled.h1`
+	color: white;
+	padding-top: 50px;
+	box-shadow: 0 -8px 6px -6px white;
+	${DeviceSizes.laptop} {
+		margin-top: 25px;
+		padding-bottom: 0;
+	}
+	${DeviceSizes.tablet} {
+		padding-bottom: 0;
+		margin-top: 25px;
+	}
+	${DeviceSizes.mobile} {
+		padding-bottom: 0;
+		margin-top: 25px;
+	}
+`;
+
+export const EducationContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 40px;
+	${DeviceSizes.laptop} {
+		padding-bottom: 20px;
+	}
+	${DeviceSizes.tablet} {
+		flex-direction: column;
+		padding-bottom: 20px;
+	}
+	${DeviceSizes.mobile} {
+		flex-direction: column;
+		padding-bottom: 20px;
+	}
+`;
+
+export const UniLogo = styled.img`
+	width: 120px;
+	${DeviceSizes.laptop} {
+		width: 100px;
+	}
+	${DeviceSizes.tablet} {
+		width: 100px;
+	}
+	${DeviceSizes.mobile} {
+		width: 100px;
+	}
+`;
+
+export const EducationDescription = styled.div`
+	font-size: 20px;
+	color: white;
+	margin-left: 15px;
+	text-align: left;
+	${DeviceSizes.laptop} {
+		font-size: 20px;
+	}
+	${DeviceSizes.tablet} {
+		font-size: 20px;
+	}
+	${DeviceSizes.mobile} {
+		font-size: 20px;
+	}
+`;
+
+export const ExperienceTitle = styled.h1`
+	color: white;
+	padding-top: 50px;
+	box-shadow: 0 -8px 6px -6px white;
+	${DeviceSizes.laptop} {
+		padding-bottom: 0;
+	}
+	${DeviceSizes.tablet} {
+		padding-bottom: 0;
+	}
+	${DeviceSizes.mobile} {
+		padding-bottom: 0;
+	}
+`;
+
+export const ExperienceContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	font-size: 20px;
+	color: white;
+	padding-bottom: 20px;
+	${DeviceSizes.laptop} {
+		flex-direction: row;
+	}
+	${DeviceSizes.tablet} {
+		flex-direction: column;
+		width: auto;
+	}
+	${DeviceSizes.mobile} {
+		flex-direction: column;
+		width: auto;
+	}
+`;
+
+export const PerlegoLogo = styled.img`
+	width: 120px;
+	height: 120px;
+	${DeviceSizes.laptop} {
+		width: 80px;
+		height: 80px;
+		margin-bottom: 10px;
+	}
+	${DeviceSizes.tablet} {
+		width: 80px;
+		height: 80px;
+		margin-bottom: 10px;
+	}
+	${DeviceSizes.mobile} {
+		width: 80px;
+		height: 80px;
+		margin-bottom: 10px;
+	}
+`;
+
+export const ExperienceDescriptionContainer = styled.div``;
+
+export const ExperienceParagraph = styled.p`
+	margin-top: 0px;
+	margin-bottom: 20px;
+	margin-right: 0px;
+	margin-left: 20px;
+	text-align: left;
+	${DeviceSizes.laptop} {
+		margin-left: 15px;
+	}
+	${DeviceSizes.tablet} {
+		margin-left: 0;
+	}
+	${DeviceSizes.mobile} {
+		margin-left: 0;
+	}
+`;
--- src/Pages/SkillsPage/SkillsPage.js
+++ src/Pages/SkillsPage/SkillsPage.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { PageContent } from "../../Components/PageContent/PageContent";
-import "./SkillsPage.css";
-import SkillsCards from "../../Components/SkillsCards/SkillsCards";
 import useContentfulHook from "../../useContentfulHook";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { FaGraduationCap, FaUserTie } from "react-icons/fa";
+import { PageContent } from "../../Components/PageContent/PageContent";
+import SkillsCards from "../../Components/SkillsCards/SkillsCards";
+import * as S from "./SkillsPage.Styled";
+// import "./SkillsPage.css";
 
 /*Collection order in Contentful: https://www.contentful.com/developers/docs/references/graphql/#/reference/collection-order */
 const query = `query {
@@ -57,49 +58,47 @@ function SkillsPage() {
 
 	return (
 		<PageContent>
-			<div id="skills-content">
-				<h1 id="skills-title">
+			<S.SkillsPageContainer>
+				<S.SkillsTitle>
 					<HiOutlineLightBulb size="50px" color="white" />
 					Skills
-				</h1>
-				<div id="skills-container">
+				</S.SkillsTitle>
+				<S.SkillsCardContainer>
 					<SkillsCards skillsCards={data.skillCardCollection.items} />
-				</div>
-				<h1 id="education-title">
+				</S.SkillsCardContainer>
+				<S.EducationTitle>
 					<FaGraduationCap size="50px" color="white" />
 					{data.educationCollection.items[0].educationTitle}
-				</h1>
-				<div id="education-container">
+				</S.EducationTitle>
+				<S.EducationContainer>
 					<div>
-						<img
-							id="uni-logo"
+						<S.UniLogo
 							src={data.educationCollection.items[0].educationImage.url}
 							alt={data.educationCollection.items[0].educationImage.title}
 						/>
 					</div>
-					<div id="education-description">
+					<S.EducationDescription>
 						{data.educationCollection.items[0].educationDescription}
-					</div>
-				</div>
-				<h1 id="experience-title">
+					</S.EducationDescription>
+				</S.EducationContainer>
+				<S.ExperienceTitle>
 					<FaUserTie size="50px" color="white" />
 					{data.experienceCollection.items[0].experienceTitle}
-				</h1>
-				<div id="experience-container">
-					<img
-						id="perlego-logo"
+				</S.ExperienceTitle>
+				<S.ExperienceContainer>
+					<S.PerlegoLogo
 						src={data.experienceCollection.items[0].companyLogo.url}
 						alt={data.experienceCollection.items[0].companyLogo.title}
 					/>
-					<div id="description">
-						<p className="skillsParagraph">
+					<S.ExperienceDescriptionContainer>
+						<S.ExperienceParagraph>
 							{documentToReactComponents(
 								data.experienceCollection.items[0].experienceDescription.json
 							)}
-						</p>
-					</div>
-				</div>
-			</div>
+						</S.ExperienceParagraph>
+					</S.ExperienceDescriptionContainer>
+				</S.ExperienceContainer>
+			</S.SkillsPageContainer>
 		</PageContent>
 	);
 }
